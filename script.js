9f787b9cbc0beaf7b420888e3fff25afa3b4259e
prasikshya <prasikshya.222baral@gmail.com>
2023-10-31 06:20:01 +0545

    New New

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const randomNumber = Math.floor(Math.random() * 5) + 1;
+const randomNumber = Math.floor(Math.random() * 100) + 1;
 
 
 const guessSubmit = document.getElementById("guessSubmit");
@@ -15,20 +15,20 @@ function checkGuess() {
     const userGuess = parseInt(guessField.value);
 
     
-    if (isNaN(userGuess) || userGuess < 1 || userGuess > 5) {
-        message.textContent = "Please enter a number between 1 and 5.";
+    if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
+        message.textContent = "Please enter a number between 1 and 100.";
     } else {
         attempts++;
 
         if (userGuess === randomNumber) {
             message.textContent = "You win!";
             guessSubmit.disabled = true;
-        } else if (attempts === 3) {
+        } else if (attempts === 10) {
             message.textContent = "You lose. The number was " + randomNumber + ".";
             guessSubmit.disabled = true;
         } else {
             const hint = userGuess < randomNumber ? "Try a higher number." : "Try a lower number.";
-            message.textContent = "Wrong guess. " + hint + " Attempts left: " + (3 - attempts);
+            message.textContent = "Wrong guess. " + hint + " Attempts left: " + (10 - attempts);
         }
     }
 
